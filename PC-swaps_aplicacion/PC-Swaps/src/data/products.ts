004c3a6dc64667fbb936b437b854c94b20b584d3
Antonio <OBT59086@educastur.es>
2026-05-08 19:11:52 +0200

    Finalizacion_documentacion

--- PC-swaps_aplicacion/PC-Swaps/src/data/products.ts
+++ PC-swaps_aplicacion/PC-Swaps/src/data/products.ts
@@ -6,29 +6,21 @@ export const products: Product[] = [
     marca: 'NVIDIA',
     modelo: 'RTX 4090',
     categoria: 'tarjeta-grafica',
-    estado: 'Como nuevo',
+    estado: 'COMO_NUEVO',
     precio: 1850,
     descripcion: '...',
     imagen: '/img/rtx.jpg',
     fechaPublicacion: '2025-03-10', // 👈 añadir
-    especificaciones: {
-      vram: '24GB',
-    },
   },
   {
     id: 2,
     marca: 'AMD',
     modelo: 'Ryzen 9 7950X3D',
     categoria: 'procesador',
-    estado: 'Nuevo',
+    estado: 'NUEVO_CON_ETIQUETAS',
     precio: 599,
     descripcion: 'Procesador de alto rendimiento.',
     imagen: '/img/ryzen.jpg',
     fechaPublicacion: '2025-04-15',
-    especificaciones: {
-      nucleos: '16',
-      hilos: '32',
-      socket: 'AM5',
-    },
   },
 ];
--- PC-swaps_aplicacion/docs/05_Objetivos.md
+++
@@ -1,9 +0,0 @@
-## 05. Objetivos
-
-1.	Facilitar la compra y venta de componentes informáticos a los usuarios.
-
-2.	Poner en contacto a compradores y vendedores de componentes informáticos.
-
-3.  Dar a los usuarios especificaciones técnicas básicas sobre los componentes informáticos que se venden en la plataforma.
-
-4.	Favorecer la economía circular de componentes informáticos.
