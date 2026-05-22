0364087e6c21ea89463ed7073b9fa0160ee7e594
Antonio <OBT59086@educastur.es>
2026-05-22 22:08:25 +0200

    Forzar despliegue Vercel

--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -1,6 +1,6 @@
 import type { EstadoArticulo } from './enums/estado-articulo';
 
-// Un slug es una version amigable de una cadena de texto.
+//Un slug es una version amigable de una cadena de texto.
 export type CategoriaSlug =
   | 'tarjeta-grafica'
   | 'placa-base'
